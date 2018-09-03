70d887eac6e64a05a1cddfed1c8cd6ca1d3f6075
Nikhil Shrivastava <nikhilshrivastava175@gmail.com>
2018-09-03 23:15:43 +0530

    Add files via upload

---
+++ arduino_code.ino
@@ -0,0 +1,20 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2, 1)
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600)
+
+}
+
+void loop() {
+  if(Serial.available())
+  {
+    delay(100);
+    lcd.clear;
+    while(Serial.avaliable() > 0)
+    {
+      lcd.write(Serial.read())
+    }
+  }
+
+}
