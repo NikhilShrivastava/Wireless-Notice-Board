e553b22d0b70a239642c2187df8eaddffc283d85
Nikhil Shrivastava <nikhilshrivastava175@gmail.com>
2018-09-11 23:39:34 +0530

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,20 +1,77 @@
-#include<LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2, 1)
-void setup() {
-  lcd.begin(16, 2);
-  Serial.begin(9600)
-
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+SoftwareSerial SIM900(9,8 );
+int inByte=0;
+void setup()
+{
+ Serial.begin(19200); // for serial monitor display
+SIM900.begin(19200); // for GSM shield start serial programming
+lcd.begin(16, 2);
+lcd.print("      ATTENTION");
+lcd.setCursor(0, 1);
+lcd.print("READ THE MESSAGE");
+delay(2000); // give time to manually power ON the GSM Shield andconnect to the network
+lcd.clear();
+SIM900.print("AT+CMGF=1\r"); // set SMS mode to text
+delay(100);
+SIM900.print("AT+CNMI=2,2,0,0,0\r"); // Configuration to extract content of the received message.Display it on Arduino Serial Monitor
+delay(100);
+// Now we simply display any text that the GSM shield sends out on the serial monitor
+do
+   {
+       while ( !SIM900.available() );   
+   } while ( '"' != SIM900.read() );
+ 
+   do
+   {
+       while ( !SIM900.available() );   
+   } while ( '"' != SIM900.read() );
+ 
+   do
+   {
+       while ( !SIM900.available() );   
+   } while ( '"' != SIM900.read() );
+ 
+   do
+   {
+       while ( !SIM900.available() );   
+   } while ( '"' != SIM900.read() );
+    do
+   {
+       while ( !SIM900.available() );   
+   } while ( '"' != SIM900.read() );
+ 
+   do
+   {
+       while ( !SIM900.available() );   
+   } while ( '"' != SIM900.read() );
+ 
+ 
+   while ( !SIM900.available() );
+   inByte = SIM900.read();   
+   while ( !SIM900.available() );
+   inByte = SIM900.read();
+   lcd.clear();
+   while(1)
+   {
+       while ( !SIM900.available() );
+       inByte = SIM900.read();
+     
+       if ( inByte == '\r' )
+         break;
+       else;
+      Serial.write(inByte);
+       lcd.write ( inByte );
+   }
 }
-
-void loop() {
-  if(Serial.available())
-  {
-    delay(100);
-    lcd.clear;
-    while(Serial.avaliable() > 0)
-    {
-      lcd.write(Serial.read())
-    }
+void loop()
+{
+  for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(150);
+  
   }
-
 }
